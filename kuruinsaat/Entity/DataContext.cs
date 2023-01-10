d763f71d8526e3420d7a3b2dfb1a5c8a0ca79bf9
serifcicek92 <serifcicek92@gmail.com>
2023-01-11 02:12:44 +0300

    entity connect

---
+++ kuruinsaat/Entity/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace kuruinsaat.Entity
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("connectionString")
+        {
+
+        }
+    }
+}
